0de4a48c5e7727ee3323ccc1b89253faa548b748
Darryl G Baker <72826250+darrylgbaker@users.noreply.github.com>
2020-11-18 13:39:20 -0500

    Add files via upload

---
+++ CetiRestart.ps1
@@ -0,0 +1,32 @@
+﻿#################################################################################
+# CetiRestart.ps1 v.1                                                           #
+#                                                                               #
+#  CetiRestart.ps1 is intended to monitor last events and restart Ceti service  #
+#  if no events have been logged in 24 hrs.                                     # 
+#                                                                               #
+#                     WRITTEN BY: Darryl G. Baker, CISSP, CCSP                  #
+#                                     for                                       #
+#                         ALSID for AD Environments                             #
+#################################################################################
+
+
+Param(
+
+[string]$uri = "",
+[string]$key = ""
+)
+
+$token = @{'x-api-key'=$key}
+
+While($true){
+    $date = Get-Date
+    $last = Invoke-RestMethod -Uri ($uri + "/api/events/last") -Method GET -Headers $token
+        
+        if($last.date -lt ($date.AddHours(-24))){
+           Get-Service alsid_Ceti | Restart-Service   
+           Start-Sleep -Seconds 86400    
+        }
+        
+ }
+ 
+  
---
+++ InvokeCetiRestart.ps1
@@ -0,0 +1,37 @@
+﻿#################################################################################
+# CetiRestart.ps1 v.1                                                           #
+#                                                                               #
+#  CetiRestart.ps1 is intended to monitor last events and restart Ceti service  #
+#  if no events have been logged in 24 hrs.                                     # 
+#                                                                               #
+#                     WRITTEN BY: Darryl G. Baker, CISSP, CCSP                  #
+#                                     for                                       #
+#                         ALSID for AD Environments                             #
+#################################################################################
+
+
+Param(
+
+[string]$uri = "",
+[string]$key = "",
+[string]$DLip = ""
+)
+
+$token = @{'x-api-key'=$key}
+
+While($true){
+    $date = Get-Date
+    $last = Invoke-RestMethod -Uri ($uri + "/api/events/last") -Method GET -Headers $token
+        
+        if($last.date -lt ($date.AddHours(-24))){
+           Invoke-Command -ComputerName $DLip -ScriptBlock{Get-Service alsid_Ceti | Restart-Service}   
+           Start-Sleep -Seconds 86400    
+        }
+        else
+        {
+            Start-Sleep 3600
+        }
+        
+ }
+ 
+  
